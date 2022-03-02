76e0c8e0a5c9b4344ca38e6d409da34111e52b8a
ansar-git95 <ansarameer01@gmail.com>
2022-03-02 17:51:16 +0500

    design the template of card-three

--- src/app/business-card-template/template-three/template-three.component.ts
+++ src/app/business-card-template/template-three/template-three.component.ts
@@ -7,6 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateThreeComponent implements OnInit {
 
+  cardStyleFront = {
+    color:'yellowgreen',
+    backgroundColor: '',
+    background:'#0000009c',
+    fontStyle: '',
+    fontFamily: 'auto',
+    width: '29%',
+    height: '13rem',
+    borderRadius: '50px 0px 50px 0px',
+    boxShadow: ' rgb(0 0 0 / 40%) 12px 10px 31px',
+    Margin: 'auto',
+  };
+  cardStyle = {
+    color: '',
+    backgroundColor: '',
+    background: 'white',
+    fontStyle: '',
+    fontFamily: 'auto',
+    width: '29%',
+    height: '80%',
+    borderRadius: '0px 50px 0px 50px',
+    boxShadow: ' rgb(0 0 0 / 40%) 18px 10px 31px',
+    Margin: 'auto',
+  };
+
   constructor() { }
 
   ngOnInit(): void {
